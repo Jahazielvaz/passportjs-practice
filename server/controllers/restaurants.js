2b8ae1ce4e244d49867229caaa15922d9ba39ae4
Jahaziel.vaz <jahaziel.vaz@gmail.com>
2018-03-16 13:45:54 -0600

    Persistance for model Restaurants is now complete

--- server/controllers/restaurants.js
+++ server/controllers/restaurants.js
@@ -1,12 +1,47 @@
 const restaurants = require('../models').Restaurants;
 
-// module.exports ={
-//   function restaurantInfo(req, res){
-//     return restaurants.create{
-//       name: req.body.name,
-//       foodType: req.body.foodType,
-//       price: req.body.price,
-//       location: req.body.location
-//     }
-//   }
-// }
+
+module.exports ={
+  create(req, res){
+    return
+    Restaurants.create({
+      name: req.body.name,
+      foodType: req.body.foodType,
+      price: req.body.price,
+      location: req.body.location
+    })
+
+    .then(console.log(req.body))
+    .catch(error, function(){
+      res.send(error);
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('development', 'KupidoExportLLC');
+// sequelize
+// .sync({
+//   force: true
+// })
+// .then(function(){
+//   Restaurants.create({
+//     name: req.body.name,
+//     foodType: req.body.foodType,
+//     price: req.body.price,
+//     location: req.body.location
+//   })
+// })
--- server/models/restaurants.js
+++ server/models/restaurants.js
@@ -1,10 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Restaurants = sequelize.define('Restaurants', {
-    name: DataTypes.STRING,
-    foodtype: DataTypes.STRING,
-    price: DataTypes.INTEGER,
-    location: DataTypes.STRING
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    foodtype: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   }, {});
   Restaurants.associate = function(models) {
     // associations can be defined here
