a03ace0dcdf3cb5af5eeb532682696be939ecc39
Jahaziel <jahaziel.vaz@gmail.com>
2018-03-16 23:25:02 -0600

    Currently working on multer. I think there's a bug I have to fix somewhere

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const bodyParser = require('body-parser');
 const urlEncoded = bodyParser.urlencoded({extended: false});
 const path = require('path');
 const passport = require('passport');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+const s3 = new aws.S3({});
+
+let upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'some-bucket',
+    metadata: function(req, file, cb){
+      cb(null, {fieldName: file.fieldname})
+    },
+    key: function(req, file, cb){
+        cb(null, Date.now().toString())
+    }
+  })
+})
 
 
 const app = express();
